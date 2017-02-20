b4edfd09d2f0fe6782ecc1c2b2c12414708b2cc5
angelo <magangde@yahoo.de>
2017-02-20 15:11:39 +0100

    SessionAttribute Listener Demo added

--- AttributesAndListenersDemo/src/java/com/maggioni/model/Dog.java
+++ AttributesAndListenersDemo/src/java/com/maggioni/model/Dog.java
@@ -1,8 +1,13 @@
 
 package com.maggioni.model;
 
-public class Dog {
+import java.util.logging.Logger;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionBindingListener;
+
+public class Dog implements HttpSessionBindingListener{
     
+    private static final Logger logger = Logger.getLogger("root.HttpSessionAttributeListener");
     private String breed;
 
     public Dog(String breed) {
@@ -12,4 +17,17 @@ public class Dog {
     public String getBreed() {
         return breed;
     } 
+
+    @Override
+    public void valueBound(HttpSessionBindingEvent event) {
+        logger.info("-->> attribute :" + event.getName() + " value :" + event.getValue() + " bound");
+        logger.info("-->> attribute :" + event.getSession().getAttribute("dog") + " bound");
+    }
+
+    @Override
+    public void valueUnbound(HttpSessionBindingEvent event) {
+        logger.info("-->> attribute :" + event.getName() + " value :" + event.getValue() + " UNbound");
+        logger.info("-->> attribute :" + event.getSession().getAttribute("dog") + " UNbound");
+    }
+
 }
